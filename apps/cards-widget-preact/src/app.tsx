7ede997bbf0855f680b519dc8025e6e056006161
AlejandroYanes <alejandro.yanes94@gmail.com>
2023-04-04 22:45:51 +0100

    PoC of iframe inside a custom web-element

--- apps/cards-widget-preact/src/app.tsx
+++ apps/cards-widget-preact/src/app.tsx
@@ -1,17 +1,12 @@
 /* eslint-disable max-len */
-import FrameContainer from './FrameContainer';
-import { useState } from 'preact/compat';
-import './Greetings';
+import './PricingCards';
 import './app.css';
 
 function App() {
-  const [name, setName] = useState('Preact');
   return (
     <main className="app">
-      <span>Testing Preact custom elements</span>
-      <input value={name} onInput={(e: any) => setName(e.target.value)} style={{ margin: '16px 0' }} />
       {/* @ts-ignore */}
-      <x-greeting name={name} />
+      <pricing-cards />
     </main>
   )
 }
