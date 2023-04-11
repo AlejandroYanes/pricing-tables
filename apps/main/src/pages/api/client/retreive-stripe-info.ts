1235c2d4501219c11d9556da49af129e6193f065
AlejandroYanes <alejandro.yanes94@gmail.com>
2023-04-11 10:03:08 +0100

    implemented a secret-key type security layer for the 3rd party endpoint #32
    closes #32

--- apps/main/src/pages/api/client/retreive-stripe-info.ts
+++ apps/main/src/pages/api/client/retreive-stripe-info.ts
@@ -1,16 +1,37 @@
+/* eslint-disable max-len */
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 import { corsMiddleware } from 'utils/api';
-import initDb from '../../../utils/planet-scale';
+import initDb from 'utils/planet-scale';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { product_id, price_id } = req.query;
-  const db = initDb();
+  const { widget_id, product_id, price_id } = req.query;
+  const userId = req.headers['X-Api-Key'] as string;
 
-  const product = await db.execute('SELECT `pricing-tables`.Product.id FROM `pricing-tables`.`Product` WHERE `mask` = ?', [product_id]);
-  const price = await db.execute('SELECT `pricing-tables`.Price.id FROM `pricing-tables`.Price WHERE `mask` = ?', [price_id]);
+  if (!userId) {
+    res.status(401).json({ error: 'Unauthorized' });
+    return;
+  }
 
-  res.status(200).json({ product, price });
+  try {
+    const db = initDb();
+
+    const product = (
+      await db.execute('SELECT `pricing-tables`.Product.id FROM `pricing-tables`.`Product` WHERE `mask` = ? AND `widgetId` = ?', [product_id, widget_id])
+    ).rows as { id: string }[];
+    const price = (
+      await db.execute('SELECT `pricing-tables`.Price.id FROM `pricing-tables`.Price WHERE `mask` = ? AND `widgetId` = ?', [price_id, widget_id])
+    ).rows as { id: string }[];
+
+    if (!product[0] || !price[0]) {
+      res.status(404).json({ error: 'Not Found' });
+      return;
+    }
+
+    res.status(200).json({ product: product[0].id, price: price[0].id });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 }
 
 export default corsMiddleware(handler);
--- apps/main/src/utils/api.ts
+++ apps/main/src/utils/api.ts
@@ -22,7 +22,7 @@ export const corsMiddleware = (next: NextHandler) => async (req: NextApiRequest,
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Api-Key'
   );
 
   if (req.method === 'OPTIONS') {
