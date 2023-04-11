f86361d4221ce399f0350da7a094a18dfff0fe0b
AlejandroYanes <alejandro.yanes94@gmail.com>
2023-04-11 17:09:25 +0100

    prepared the card-widget project to be deployed #36

--- apps/cards-widget/src/main.tsx
+++ apps/cards-widget/src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
 import './PricingCards';
 
-ReactDOM.createRoot(document.getElementById('pricing-cards') as HTMLElement).render(
-  <React.StrictMode>
-    {/* @ts-ignore */}
-    <pricing-cards widget="clgan0d7s0001jed6a5epfphh" theme="light" />
-  </React.StrictMode>,
-);
+// ReactDOM.createRoot(document.getElementById('pricing-cards') as HTMLElement).render(
+//   <React.StrictMode>
+//     {/* @ts-ignore */}
+//     <pricing-cards widget="clgan0d7s0001jed6a5epfphh" theme="light" />
+//   </React.StrictMode>,
+// );
