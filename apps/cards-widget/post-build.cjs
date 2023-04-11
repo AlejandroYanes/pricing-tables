10591daa221f13548200ced071b91f6807440f7a
AlejandroYanes <alejandro.yanes94@gmail.com>
2023-04-11 18:49:58 +0100

    reset the version to 0.0.1 and added a copy for the index.html file #36

--- apps/cards-widget/post-build.cjs
+++ apps/cards-widget/post-build.cjs
@@ -2,3 +2,4 @@ const shell = require('shelljs');
 
 // Move the files from the build output folder to the desired folder
 shell.mv('dist/assets/*', 'scripts/');
+shell.mv('dist/index.html', 'scripts/index.html');
