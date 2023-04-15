7e15d1c8772014419e2e096024152e348e022d1f
AlejandroYanes <alejandro.yanes94@gmail.com>
2023-04-15 23:00:29 +0100

    updated the url used on the custom element to fetch the widget info

--- apps/cards-widget/src/PricingCards.tsx
+++ apps/cards-widget/src/PricingCards.tsx
@@ -34,7 +34,7 @@ const PricingCards = (props: Props) => {
   useEffect(() => {
     if (widget) {
       callAPI({
-        url: `http://localhost:3000/api/client/widget/${widget}`,
+        url: `https://pricing-tables.vercel.app/api/client/widget/${widget}`,
         method: 'GET',
       })
         .then((res) => {
