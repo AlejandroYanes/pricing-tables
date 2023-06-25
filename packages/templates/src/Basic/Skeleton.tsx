e67c3a17ef982e6b6386bc9ac4b8b28b68af6762
AlejandroYanes <alejandro.yanes94@gmail.com>
2023-06-25 14:51:44 +0100

    DEV-63: moved the Skeletons to tailwind styles

--- packages/templates/src/Basic/Skeleton.tsx
+++ packages/templates/src/Basic/Skeleton.tsx
@@ -11,23 +11,23 @@ export function BasicSkeleton(props: SkeletonProps) {
     <div className="flex flex-col items-center">
       <div className="grid grid-rows-3 justify-items-center box-border gap-4">
         {arr.map((_, index) => (
-          <Skeleton key={index} animate={false} height={300 * scale} width={300 * scale} />
+          <div key={index} className="rounded-sm bg-slate-200" style={{ height: 300 * scale, width: 300 * scale }} />
         ))}
         {arr.fill(0).map((_, index) => (
-          <Stack key={index} pl={32 * scale} style={{ width: '100%' }}>
-            <Group spacing="xs">
-              <Skeleton circle animate={false} height={20 * scale} width={20 * scale} />
-              <Skeleton animate={false} height={20 * scale} width={170 * scale} />
-            </Group>
-            <Group spacing="xs">
-              <Skeleton circle animate={false} height={20 * scale} width={20 * scale} />
-              <Skeleton animate={false} height={20 * scale} width={170 * scale} />
-            </Group>
-            <Group spacing="xs">
-              <Skeleton circle animate={false} height={20 * scale} width={20 * scale} />
-              <Skeleton animate={false} height={20 * scale} width={170 * scale} />
-            </Group>
-          </Stack>
+          <div key={index} className="flex flex-col w-full" style={{ paddingLeft: 32 * scale }}>
+            <div className="flex items-center gap-2">
+              <div className="rounded-full bg-slate-200" style={{ height: 20 * scale, width: 20 * scale }} />
+              <div className="rounded-sm bg-slate-200" style={{ height: 20 * scale, width: 170 * scale }} />
+            </div>
+            <div className="flex items-center gap-2">
+              <div className="rounded-full bg-slate-200" style={{ height: 20 * scale, width: 20 * scale }} />
+              <div className="rounded-sm bg-slate-200" style={{ height: 20 * scale, width: 170 * scale }} />
+            </div>
+            <div className="flex items-center gap-2">
+              <div className="rounded-full bg-slate-200" style={{ height: 20 * scale, width: 20 * scale }} />
+              <div className="rounded-sm bg-slate-200" style={{ height: 20 * scale, width: 170 * scale }} />
+            </div>
+          </div>
         ))}
       </div>
     </div>
