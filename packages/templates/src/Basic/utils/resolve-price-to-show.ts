af074791475102f4d664d0ce461d8302b2bbaf64
fabiannediaz <diazfabianne@gmail.com>
2023-06-27 18:10:08 +0100

    displaying the cards for custom intervals

--- packages/templates/src/Basic/utils/resolve-price-to-show.ts
+++ packages/templates/src/Basic/utils/resolve-price-to-show.ts
@@ -8,8 +8,10 @@ export const resolvePriceToShow = (prod: FormProduct, interval: Interval) => {
   if (interval === 'one_time') return prod.prices.find((price) => price.type === 'one_time')!;
 
   return prod.prices.find((price) => {
-    if (interval === 'month_3') return price.recurring?.interval_count === 3;
-    if (interval === 'month_6') return price.recurring?.interval_count === 6;
+    if (interval.includes('_')) {
+      const intervalArray = interval.split('_');
+      return price.recurring?.interval_count.toString() === intervalArray[1];
+    };
 
     return price.recurring?.interval === interval;
   })!;
