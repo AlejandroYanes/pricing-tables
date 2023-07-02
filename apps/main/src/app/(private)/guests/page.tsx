b5ae4be502ce4619287f6331bde7f815fae97aec
AlejandroYanes <alejandro.yanes94@gmail.com>
2023-07-02 11:50:05 +0100

    DEV-66: finished the users & guess pages/tables

--- apps/main/src/app/(private)/guests/page.tsx
+++ apps/main/src/app/(private)/guests/page.tsx
@@ -1,46 +1,19 @@
-'use client'
-
-import { useState } from 'react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { Button, Group, Stack } from '@mantine/core';
 
-import { trpc } from 'utils/trpc';
 import BaseLayout from 'components/BaseLayout';
 import GuestsTable from 'components/GuestsTable';
 
 const UsersPage: NextPage = () => {
-  const [page, setPage] = useState(1);
-  const [pageSize, setPageSize] = useState(25);
-
-  const {
-    data: { results, count } = { results: [], count: 0 },
-    refetch,
-  } = trpc.widgets.listGuestWidgets.useQuery({ page, pageSize }, { keepPreviousData: true });
-
-  const { mutate, isLoading } = trpc.widgets.deleteGuestWidgets.useMutation({
-    onSuccess: () => refetch(),
-  });
-
   return (
     <>
       <Head>
         <title>Dealo | Guest Users</title>
       </Head>
       <BaseLayout showBackButton backRoute="/dashboard" title="Guest Users">
-        <Stack mx="auto" sx={{ maxWidth: '980px', width: '100%' }}>
-          <Group position="right" mb="xl">
-            <Button color="red" variant="outline" loading={isLoading} onClick={() => mutate()}>Delete All</Button>
-          </Group>
-          <GuestsTable
-            page={page}
-            pageSize={pageSize}
-            count={count}
-            data={results}
-            onPageChange={setPage}
-            onPageSizeChange={setPageSize}
-          />
-        </Stack>
+        <div className="flex flex-col mx-auto max-w-[980px] w-full">
+          <GuestsTable />
+        </div>
       </BaseLayout>
     </>
   );
--- apps/main/src/components/UsersTable/index.tsx
+++ apps/main/src/components/UsersTable/index.tsx
@@ -1,25 +1,9 @@
 'use client'
 
 import { useState } from 'react';
-import {
-  Badge,
-  // Group,
-  // Pagination,
-  // Select,
-  Table,
-  // Text,
-  TextInput,
-  // createStyles,
-  // Popover,
-  // ActionIcon,
-  // Stack,
-  // Divider,
-  // Radio,
-} from '@mantine/core';
 import { IconAdjustments } from '@tabler/icons-react';
-import { calculateTotal } from '@dealo/helpers';
-import { RenderIf } from '@dealo/ui';
 import { useDebouncedState } from '@mantine/hooks';
+import { RenderIf } from '@dealo/ui';
 
 import { trpc } from 'utils/trpc';
 import UserAvatar from 'components/UserAvatar';
@@ -31,8 +15,17 @@ import {
 import { Button } from 'components/ui/button';
 import { Label } from 'components/ui/label';
 import { RadioGroup, RadioGroupItem } from 'components/ui/radio-group';
-import Pagination from '../ui/pagination';
-import { Separator } from '../ui/separator';
+import Pagination from 'components/ui/pagination';
+import { Input } from 'components/ui/input';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from 'components/ui/table';
+import { Badge } from 'components/ui/badge';
 
 const UsersTable = () => {
   const [query, setQuery] = useDebouncedState('', 200);
@@ -49,46 +42,12 @@ const UsersTable = () => {
     setPage(1);
   };
 
-  const rows = results.map((user) => (
-    <tr key={user.id}>
-      <td>
-        <div className="flex items-center gap-2">
-          <UserAvatar user={user} />
-          <div className="flex flex-col">
-            <span className="text-lg font-semibold">
-              {user.name ?? 'Anonymous'}
-            </span>
-            <span className="text-sm text-muted-foreground">
-              {user.email ?? 'no email'}
-            </span>
-          </div>
-        </div>
-      </td>
-
-      <td>
-        <RenderIf
-          condition={user.isSetup}
-          fallback={
-            <Badge color="orange">No</Badge>
-          }
-        >
-          <Badge color="green">Yes</Badge>
-        </RenderIf>
-      </td>
-
-      <td>{user.isSetup ? user._count.widgets : 'N/A'}</td>
-    </tr>
-  ));
-
   return (
     <>
-      <div className="flex justify-between items-center">
-        <TextInput
-          my="lg"
-          mr="auto"
-          defaultValue=""
+      <div className="flex justify-between items-center mb-4">
+        <Input
           placeholder="Search users"
-          sx={{ width: '280px' }}
+          className="w-[280px]"
           onChange={(e) => setQuery(e.target.value)}
         />
         <Popover>
@@ -124,27 +83,55 @@ const UsersTable = () => {
           </PopoverContent>
         </Popover>
       </div>
-      <Table verticalSpacing="sm" horizontalSpacing="lg">
-        <thead>
-          <tr>
-            <th>User</th>
-            <th style={{ width: '100px' }}>Is Setup</th>
-            <th style={{ width: '130px' }}>Widgets</th>
-          </tr>
-        </thead>
-        <tbody>{rows}</tbody>
-      </Table>
-      <div className="flex items-center justify-between py-6 sticky bottom-0">
-        <div className="flex items-center gap-4">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>User</TableHead>
+            <TableHead style={{ width: '100px' }}>Is Setup</TableHead>
+            <TableHead style={{ width: '130px' }}>Widgets</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {results.map((user) => (
+            <TableRow key={user.id}>
+              <TableCell>
+                <div className="flex items-center gap-2">
+                  <UserAvatar user={user} />
+                  <div className="flex flex-col">
+                    <span className="text-lg font-semibold">
+                      {user.name ?? 'Anonymous'}
+                    </span>
+                    <span className="text-sm text-muted-foreground">
+                      {user.email ?? 'no email'}
+                    </span>
+                  </div>
+                </div>
+              </TableCell>
 
-          <span className="text-sm font-medium">{`Total: ${count}`}</span>
-        </div>
+              <TableCell>
+                <RenderIf
+                  condition={user.isSetup}
+                  fallback={
+                    <Badge variant="secondary">No</Badge>
+                  }
+                >
+                  <Badge variant="success">Yes</Badge>
+                </RenderIf>
+              </TableCell>
+
+              <TableCell>{user.isSetup ? user._count.widgets : 'N/A'}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <div className="flex items-center justify-between py-6 sticky bottom-0 bg-white">
+        <span className="text-sm font-medium">{`Total: ${count}`}</span>
         <Pagination
           page={page}
           onPageChange={setPage}
           pageSize={pageSize}
           onPageSizeChange={setPageSize}
-          total={calculateTotal(count, pageSize)}
+          total={count}
         />
       </div>
     </>
