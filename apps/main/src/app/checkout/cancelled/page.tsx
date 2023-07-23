812c7c9ff38d3ed35fbe0d4e1bef42c6370c6102
AlejandroYanes <alejandro.yanes94@gmail.com>
2023-07-23 23:16:46 +0100

    DEV-66: migrated the stock checkout result pages to tailwind

--- apps/main/src/app/checkout/cancelled/page.tsx
+++ apps/main/src/app/checkout/cancelled/page.tsx
@@ -1,4 +1,3 @@
-import { Group, Stack, Title } from '@mantine/core';
 import { IconMoodConfuzed } from '@tabler/icons-react';
 
 import BaseLayout from 'components/BaseLayout';
@@ -6,12 +5,12 @@ import BaseLayout from 'components/BaseLayout';
 export default function CheckoutError() {
   return (
     <BaseLayout hideNavbar>
-      <Stack align="center" justify="center" style={{ height: '100vh' }}>
-        <Group>
+      <div className="flex flex-col text-center justify-center h-[100vh]">
+        <div className="flex">
           <IconMoodConfuzed size={88} />
-          <Title order={3} size={36} w={360}>Payment was cancelled.</Title>
-        </Group>
-      </Stack>
+          <h3 className="text text-[36px] w-[360px]">Payment was cancelled.</h3>
+        </div>
+      </div>
     </BaseLayout>
   );
 }
--- apps/main/src/app/checkout/error/page.tsx
+++ apps/main/src/app/checkout/error/page.tsx
@@ -1,4 +1,3 @@
-import { Anchor, Group, Stack, Title } from '@mantine/core';
 import { IconAlertTriangle } from '@tabler/icons-react';
 
 import BaseLayout from 'components/BaseLayout';
@@ -6,16 +5,16 @@ import BaseLayout from 'components/BaseLayout';
 export default function CheckoutError() {
   return (
     <BaseLayout hideNavbar>
-      <Stack align="center" justify="center" style={{ height: '100vh' }}>
-        <Group>
+      <div className="flex flex-col items-center justify-center h-[100vh]">
+        <div className="flex">
           <IconAlertTriangle size={88} />
-          <Title order={3} w={480}>
+          <h3 className="text text-[36px] w-[360px]">
             Something went wrong, please contact support at {' '}
-            <Anchor href="mailto:alejandro.yanes94@gmail.com">alejandro.yanes94@gmail.com</Anchor>
+            <a href="mailto:alejandro.yanes94@gmail.com">alejandro.yanes94@gmail.com</a>
             .
-          </Title>
-        </Group>
-      </Stack>
+          </h3>
+        </div>
+      </div>
     </BaseLayout>
   );
 }
