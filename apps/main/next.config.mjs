c42304f4c4d43d0aa123b18989f9ad4a84100ddb
AlejandroYanes <alejandro.yanes94@gmail.com>
2023-12-21 20:02:11 +0000

    feat: Add axiom as log platform

--- apps/main/next.config.mjs
+++ apps/main/next.config.mjs
@@ -4,9 +4,11 @@
  * This is especially useful for Docker builds.
  */
 !process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
+import { withAxiom } from 'next-axiom';
+
 
 /** @type {import("next").NextConfig} */
-const config = {
+const config = withAxiom({
   reactStrictMode: true,
   /* If trying out the experimental appDir, comment the i18n config out
    * @see https://github.com/vercel/next.js/issues/41980 */
@@ -15,5 +17,5 @@ const config = {
     defaultLocale: "en",
   },
   transpilePackages: ["ui", "templates", "helpers", "models"],
-};
+});
 export default config;
