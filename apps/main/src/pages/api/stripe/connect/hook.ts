883f655070cdd4bad198d5c28b4c829b9de70563
AlejandroYanes <alejandro.yanes94@gmail.com>
2023-12-22 18:10:43 +0000

    fix: removed `pricing-table` leading key on SQL queries

--- apps/main/src/pages/api/stripe/connect/hook.ts
+++ apps/main/src/pages/api/stripe/connect/hook.ts
@@ -13,10 +13,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (charges_enabled) {
       const db = initDb();
       await db.transaction(async (tx) => {
-        await tx.execute('UPDATE `pricing-tables`.`User` SET `stripeConnected` = true, `stripeKey` = null WHERE `stripeAccount` = ?', [id]);
+        await tx.execute('UPDATE User SET stripeConnected = true, stripeKey = null WHERE stripeAccount = ?', [id]);
       });
       const { name } = (
-        await db.execute('SELECT name FROM `pricing-tables`.`User` WHERE `stripeAccount` = ?', [id])
+        await db.execute('SELECT name FROM User WHERE stripeAccount = ?', [id])
       ).rows[0] as { name: string };
       notifyOfNewSetup({ name });
     }
