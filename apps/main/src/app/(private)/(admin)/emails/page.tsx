a6374b1b16139d3120246a06ed1a17fc3dbfa748
AlejandroYanes <alejandro.yanes94@gmail.com>
2024-01-15 09:11:07 +0000

    implemented the email addresses collection

--- apps/main/src/app/(private)/(admin)/emails/page.tsx
+++ apps/main/src/app/(private)/(admin)/emails/page.tsx
@@ -1,9 +1,9 @@
 import { render } from '@react-email/render';
-import { InputWithLabel } from '@dealo/ui';
 import { NewReleaseEmail } from '@dealo/email-templates';
 
 import { env } from 'env/server.mjs';
 import BaseLayout from 'components/BaseLayout';
+import EmailForm from './EmailForm';
 
 const URL = env.PLATFORM_URL || `https://${process.env.VERCEL_URL}`;
 const logoImage = `${URL}/logo/dealo_logo_block.png`;
@@ -13,11 +13,11 @@ const EmailsPage = () => {
 
   return (
     <BaseLayout title="Email">
-      <div className="flex flex-col items-center">
-        <div className="flex flex-col gap-6 min-w-[700px]">
-          <InputWithLabel label="To"/>
-          <InputWithLabel label="Subject"/>
-          <h3 className="text-2xl text-left">Preview:</h3>
+      <div className="flex gap-8">
+        <div className="flex flex-col gap-6 w-[35%]">
+          <EmailForm />
+        </div>
+        <div className="flex flex-col gap-6 mx-auto min-w-[700px]">
           <iframe srcDoc={markup} className="w-full h-[calc(100vh_-_70px)]"/>
         </div>
       </div>
