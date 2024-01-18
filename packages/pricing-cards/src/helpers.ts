2f6781aa6b8730fdd9f6b111a7b34684d7404352
AlejandroYanes <alejandro.yanes94@gmail.com>
2024-01-18 08:14:14 +0000

    DEV-91: removed the hardcoded domain

--- packages/pricing-cards/src/helpers.ts
+++ packages/pricing-cards/src/helpers.ts
@@ -1,5 +1,4 @@
 export function resolveDomain(internal: boolean) {
-  // const currentUrl = new URL(window.location.href);
-  // return internal ? currentUrl.origin : 'https://dealo.app';
-  return 'http://localhost:3000';
+  const currentUrl = new URL(window.location.href);
+  return internal ? currentUrl.origin : 'https://dealo.app';
 }
