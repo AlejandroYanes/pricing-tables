d605c433d9be70baac9951c52f1ee9d6331c2281
AlejandroYanes <alejandro.yanes94@gmail.com>
2024-01-29 22:22:49 +0000

    wrapped the pricing page blocks that use the client side session

--- apps/main/src/app/pricing/page.tsx
+++ apps/main/src/app/pricing/page.tsx
@@ -8,6 +8,7 @@ import { authOptions } from 'utils/auth';
 import BaseLayout from 'components/base-layout';
 import PricingWidget from 'components/pricing-widget';
 import { NavbarLink } from 'components/navbar';
+import ClientProviders from 'components/client-providers';
 
 export const metadata: Metadata = {
   title: 'Dealo | Pricing',
@@ -66,23 +67,27 @@ export default async function PricingPage() {
 
   if (hasSubscription) {
     return (
-      <BaseLayout hideUserControls showBackButton>
-        <div className="flex flex-col items-center justify-center gap-6 mt-[48px] mx-auto h-[calc(100vh - 88px)]">
-          <h1 className="text text-2xl">Hi {user.name}</h1>
-          <span className="text">You already have a subscription with us, if you want to check:</span>
-          <Link href="/api/stripe/customer/portal">
-            <Button>Got to the Customer Portal</Button>
-          </Link>
-        </div>
-      </BaseLayout>
+      <ClientProviders session={session}>
+        <BaseLayout hideUserControls showBackButton>
+          <div className="flex flex-col items-center justify-center gap-6 mt-[48px] mx-auto h-[calc(100vh_-_88px)]">
+            <h1 className="text text-2xl">Hi {user.name}</h1>
+            <span className="text">You already have a subscription with us, if you want to check:</span>
+            <Link href="/api/stripe/customer/portal">
+              <Button>Got to the Customer Portal</Button>
+            </Link>
+          </div>
+        </BaseLayout>
+      </ClientProviders>
     );
   }
 
   return (
-    <BaseLayout hideUserControls showBackButton>
-      <div className="flex flex-col items-center justify-center mx-auto h-[calc(100vh - 88px)]">
-        <PricingWidget />
-      </div>
-    </BaseLayout>
+    <ClientProviders session={session}>
+      <BaseLayout hideUserControls showBackButton>
+        <div className="flex flex-col items-center justify-center mx-auto h-[calc(100vh_-_88px)]">
+          <PricingWidget />
+        </div>
+      </BaseLayout>
+    </ClientProviders>
   );
 }
