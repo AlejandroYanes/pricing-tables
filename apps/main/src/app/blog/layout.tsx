cc0b95351bad15885b71cc9e045b683d92d05471
AlejandroYanes <alejandro.yanes94@gmail.com>
2024-02-03 13:23:18 +0000

    DEV-103: updated name of layout component

--- apps/main/src/app/blog/layout.tsx
+++ apps/main/src/app/blog/layout.tsx
@@ -1,7 +1,7 @@
 import BaseLayout from 'components/base-layout';
 import PublicNavbar from 'components/public-navbar';
 
-export default function MdxLayout({ children }: { children: any }) {
+export default function BlogsLayout({ children }: { children: any }) {
   return (
     <BaseLayout hideNavbar footerClassName="w-full max-w-[1200px] mx-auto">
       <PublicNavbar showHome />
